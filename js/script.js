0d913790b681480ffc5ed72c3b2f6f5fa8c57448
kaito205 <bochilandi85@gmail.com>
2024-09-21 23:43:43 +0700

    commit todo list

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const timestamp = document.getElementById('date').value;
 
     const generatedID = generatedId();
-    const todoObJect = generatTodoObject(generatedID, textTodo, timestamp, false);
+    const todoObJect = generateTodoObject(generatedID, textTodo, timestamp, false);
     todos.push(todoObJect);
 
     document.dispatchEvent(new Event(RENDER_EVENT));
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
     return +new Date();
   }
 
-  function generatTodoObject(id, task, timestamp, isCompleted) {
+  function generateTodoObject(id, task, timestamp, isCompleted) {
     return {
       id,
       task,
@@ -35,12 +35,12 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 
-  function makeTodo(todoObJect) {
+  function makeTodo(todoObject) {
     const textTitle = document.createElement('h2');
-    textTitle.innerText = todoObJect.task;
+    textTitle.innerText = todoObject.task;
 
     const textTimestamp = document.createElement('p');
-    textTimestamp.innerText = todoObJect.timestamp;
+    textTimestamp.innerText = todoObject.timestamp;
 
     const textContainer = document.createElement('div');
     textContainer.append(textTitle, textTimestamp);
@@ -48,21 +48,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const container = document.createElement('div');
     container.classList.add('item', 'shadow');
     container.append(textContainer);
-    container.setAttribute('id', `todo-${todoObJect.id}`);
+    container.setAttribute('id', `todo-${todoObject.id}`);
 
-    if(todoObJect.isCompleted) {
+    if(todoObject.isCompleted) {
       const undoButton = document.createElement('button');
-      undoButton.classList.add('undo-butoon');
+      undoButton.classList.add('undo-button');
 
       undoButton.addEventListener('click', function() {
-        undoTaskFromCompleted(todoObJect.id);
+        undoTaskFromCompleted(todoObject.id);
       });
 
       const trashButton = document.createElement('button');
       trashButton.classList.add('trash-button');
 
       trashButton.addEventListener('click', function() {
-        removeTaskFromCompleted(todoObJect.id);
+        removeTaskFromCompleted(todoObject.id);
       });
 
       container.append(undoButton, trashButton);
@@ -71,7 +71,7 @@ document.addEventListener('DOMContentLoaded', function() {
       checkButton.classList.add('check-button');
 
       checkButton.addEventListener('click', function() {
-        addTaskToCompleted(todoObJect.id);
+        addTaskToCompleted(todoObject.id);
       });
 
       container.append(checkButton);
@@ -85,30 +85,67 @@ document.addEventListener('DOMContentLoaded', function() {
       todoTarget.isCompleted = true;
       document.dispatchEvent(new Event(RENDER_EVENT));
     }
-
-    function findTodo(todoId) {
-      for(const todoItem of todos) {
-        if(todoItem.id === todoId) {
-          return todoItem;
-        }
-      }
-
-      return null;
-    }
-
     return container;
   }
+
+  function findTodo(todoId) {
+    for(const todoItem of todos) {
+      if(todoItem.id === todoId) {
+        return todoItem;
+      }
+    }
+
+    return null;
+  }
+
+  function removeTaskFromCompleted(todoId) {
+    const todoTarget = findTodoIndex(todoId);
+
+    if(todoTarget === -1) return;
+
+    todos.splice(todoTarget, 1);
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+
+  function undoTaskFromCompleted(todoId) {
+    const todoTarget = findTodo(todoId);
+
+    if(todoTarget == null) return;
+
+    todoTarget.isCompleted = false;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+
+  function findTodoIndex(todoId) {
+    for(const index in todos) {
+      if(todos[index].id === todoId) {
+        return index;
+      }
+    }
+
+    return -1;
+  }
+
+  
 
   document.addEventListener(RENDER_EVENT, function() {
     const uncompletedTODOList = document.getElementById('todos');
     uncompletedTODOList.innerHTML = '';
 
+    const completedTODOList = document.getElementById('completed-todos');
+    completedTODOList.innerHTML = '';
+
+
     for(const todoItem of todos) {
       const todoElement = makeTodo(todoItem);
       if(!todoItem.isCompleted) {
         uncompletedTODOList.append(todoElement);
+      } else {
+        completedTODOList.append(todoElement);
       }
     }
   });
+
+
   
 });
