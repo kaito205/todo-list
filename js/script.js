a62f2cdf1fbbada8e96c535cee9ff51346a429a0
kaito205 <bochilandi85@gmail.com>
2024-09-25 00:16:46 +0700

    commit todo list

--- js/script.js
+++ js/script.js
@@ -128,7 +128,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
   
 
- 
+  document.addEventListener(RENDER_EVENT, function() {
+    const uncompletedTODOList = document.getElementById('todos');
+    uncompletedTODOList.innerHTML = '';
+
+    const completedTODOList = document.getElementById('completed-todos');
+    completedTODOList.innerHTML = '';
+
+
+    for(const todoItem of todos) {
+      const todoElement = makeTodo(todoItem);
+      if(!todoItem.isCompleted) {
+        uncompletedTODOList.append(todoElement);
+      } else {
+        completedTODOList.append(todoElement);
+      }
+    }
   });
   
 });
